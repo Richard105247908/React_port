b8b6339b44d064415a2e9d871d10afd792e80b1e
Richard105247908 <105247908@aston.ac.uk>
2024-03-22 14:07:12 +0000

    import images

--- src/components/data.jsx
+++ src/components/data.jsx
@@ -1,45 +1,52 @@
+import Meal from '../components/images/meal.png'
+import Bootstrap from '../components/images/bootstrap.png'
+import Consolefinances from '../components/images/finances.png'
+import Workschedule from '../components/images/workschedule.png'
+import Password from '../components/images/Password.png'
+import Codequiz from '../components/images/Code-Quiz.png'
+
 export default[
     {
       "id": 1,
       "title": "Student Recipes",
       "deployedLink": "https://richard105247908.github.io/student-recipes/",
       "githubLink": "https://github.com/Richard105247908/student-recipes",
-      "image": "src/components/images/meal.png"
+      "image": Meal
     },
     {
       "id": 2,
       "title": "Bootstrap Portfolio ",
       "deployedLink": "https://richard105247908.github.io/Bootstrap-portfolio/",
       "githubLink": "https://github.com/Richard105247908/Bootstrap-portfolio",
-      "image": "src/components/images/bootstrap.png"
+      "image": Bootstrap
     },
     {
       "id": 3,
       "title": "Console Finances",
       "deployedLink": "https://richard105247908.github.io/Console-finances/",
       "githubLink": "https://github.com/Richard105247908/Console-finances",
-      "image": "src/components/images/finances.png"
+      "image": Consolefinances
     },
     {
       "id": 4,
       "title": "Work Day Scheduler",
       "deployedLink": "https://richard105247908.github.io/Third_Party-APIs-Work-Day-Scheduler/",
       "githubLink": "https://github.com/Richard105247908/Third_Party-APIs-Work-Day-Scheduler",
-      "image": "src/components/images/workschedule.png"
+      "image": Workschedule
     },
     {
       "id": 5,
       "title": "Password Generator",
       "deployedLink": "https://richard105247908.github.io/passgen/",
       "githubLink": "https://github.com/Richard105247908/passgen",
-      "image": "src/components/images/Password.png"
+      "image": Password
     },
     {
       "id": 6,
       "title": "Code quiz",
       "deployedLink": "https://richard105247908.github.io/code-quiz/",
       "githubLink": "https://github.com/Richard105247908/code-quiz",
-      "image": "src/components/images/Code-Quiz.png"
+      "image": Codequiz
     }
     
 ]
